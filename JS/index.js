985226536b986d7b13a86a6231d6d6c94f840a43
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-10-25 12:50:09 +0200

    changes from jade-iput fields validation and local storage

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function () {
     $.ajax({
         url: 'https://api.themoviedb.org/3/trending/movie/day?api_key=33a910bf405b0e95e4f78d2f4f9b1567&primary_release_date=1990-10-12',
@@ -83,3 +84,35 @@ loadMovieCards = (movies) => {
         window.location.href = `pages/movie.html?id=${$(this).find('.card').attr('data-id')}`;
     })
 }
+=======
+/////////////////////////////////////////////////////////////////////////
+///////////sign up page and sign in page//////////
+$(document).ready(function() {
+  //-----------------------------------------------------------
+  //-sign up form------------------------------
+  //-----------------------------------------------------------
+  $('#sign-up-form').submit(function(event) {
+    event.preventDefault();
+    if (this.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      // Check if the password meets the criteria
+      const password = $('#password').val();
+      if (isPasswordValid(password)) {
+        // Any code that should run when the password is valid
+        window.location.href = 'pages/index.html';
+      } else {
+        // Display an error message or take appropriate action
+        alert('Password must contain at least one capital letter and one number.');
+      }
+    }
+    $(this).addClass('was-validated');
+  });
+
+  function isPasswordValid(password) {
+    // Define a regular expression to check for a capital letter and a number
+    const regex = /^(?=.*[A-Z])(?=.*\d)/;
+    return regex.test(password);
+  }
+});
+>>>>>>> jade
