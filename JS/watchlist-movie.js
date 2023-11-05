0c0112419d33dd1daccdc5bc5050b9cc7c4bf540
Megan Beyl <231206@virtualwindow.co.za>
2023-11-05 21:02:19 +0200

    Restarting JS

--- JS/watchlist-movie.js
+++ JS/watchlist-movie.js
@@ -1,68 +1,23 @@
 // --------------------------------------------------------------------
 // When the page loads
-$(document).ready(function (){
 
-    // Load in movie information
-    let data = JSON.parse(localStorage.getItem(''));
-    let movieItems = document.getElementById('');
-
-    for (let i = 0; i < data.length; i++){
-        let image = data[i].movieThumb;
-        let title = data[i].movieTitle;
-        let time = data[i].movieRunTime;
-        let caption = data[i].movieCaption;
-        let genre = data[i].movieGenre;
-        let trailer = data[i].movieTrailer;
-        let poster = data[i].moviePoster;
-        let poster2 = data[i].moviePoster2;
-
-        movieItems.innerHTML += `
-        
-        `
-
-
-    }
-
-    // Load in trailers
+    // Load in trailers, etc.
     $.ajax({
         url: 'https://api.themoviedb.org/3/movie/11?api_key=4bdfdd167440b11eee82c213da4dd90b',
         type: 'GET',
         dataType: 'json',
         success: function(data) {
-            let movies = data;
+            let movies = data.results;
             console.log(movies);
             loadMovies(movies);
         }
     })
 
-    function loadMovies(moviesToShow) {
+$(document).ready(function (){
 
-
-    
-        // load in movies 2?
-    
-        for (let i = 0; i < moviesToShow.length; i++) {
-            const movie = moviesToShow[i];
-    
-            console.log(trip);
-    
-            // 1: Select the movie container and add the current movie to it
-            $("#movieContainer").append($("#movieWatchCardTemplate").html());
-    
-            // 2. Create a variable that contains the most recently added movie card
-            let currentChild = $("#movieContainer").children().eq(i);
-    
-            // 3. Set the content for the current trip card from the trip array
-            $(currentChild).find("#movieTitle").text(trip.name);
-            $(currentChild).find("#amountText").text(trip.price);
-            $(currentChild).find("#informationText").text(trip.caption);
-            $(currentChild).find("#dateCruise").text(trip.dateOfCruise);
-            $(currentChild).find(".card-img-top").attr('src', '../assets/' + trip.image);
-    
-            
-            
-        };
-    }
+    // Load in movie information
+    let data = JSON.find('#movieCard');
+    let movieItems = document.getElementById('');
 
 
 
