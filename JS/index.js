9f4b153340e8b29b88adbc83d049eab6e74c9638
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-11-07 11:40:45 +0200

    merge solving

--- JS/index.js
+++ JS/index.js
@@ -86,31 +86,31 @@ loadMovieCards = (movies) => {
 /////////////////////////////////////////////////////////////////////////
 ///////////sign up page and sign in page//////////
 $(document).ready(function() {
-  //-----------------------------------------------------------
-  //-sign up form------------------------------
-  //-----------------------------------------------------------
-  $('#sign-up-form').submit(function(event) {
-    event.preventDefault();
-    if (this.checkValidity() === false) {
-      event.stopPropagation();
-    } else {
-      // Check if the password meets the criteria
-      const password = $('#password').val();
-      if (isPasswordValid(password)) {
-        // Any code that should run when the password is valid
-        window.location.href = 'pages/index.html';
+    //-----------------------------------------------------------
+    //- Form Submit Logic (for both sign-up and sign-in) -------
+    //-----------------------------------------------------------
+    $('form').submit(function(event) {
+      event.preventDefault();
+      if (this.checkValidity() === false) {
+        event.stopPropagation();
       } else {
-        // Display an error message or take appropriate action
-        alert('Password must contain at least one capital letter and one number.');
+        // Check if the password meets the criteria
+        const password = $('#password').val();
+        if (isPasswordValid(password)) {
+          // Any code that should run when the password is valid
+          window.location.href = 'pages/index.html';
+        } else {
+          // Display an error message under the input field
+          $('#password-error').text('Password must contain at least one capital letter and one number.');
+          $('#password').addClass('is-invalid');
+        }
       }
+      $(this).addClass('was-validated');
+    });
+  
+    function isPasswordValid(password) {
+      // Define a regular expression to check for a capital letter and a number
+      const regex = /^(?=.*[A-Z])(?=.*\d)/;
+      return regex.test(password);
     }
-    $(this).addClass('was-validated');
   });
-
-  function isPasswordValid(password) {
-    // Define a regular expression to check for a capital letter and a number
-    const regex = /^(?=.*[A-Z])(?=.*\d)/;
-    return regex.test(password);
-  }
-
-});
