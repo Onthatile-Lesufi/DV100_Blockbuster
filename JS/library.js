a9c4a7e054933dc61c894b4783c05557892398d2
Onthatile-Lesufi <231262@virtualwindow.co.za>
2023-11-07 17:12:08 +0200

    add filters

--- JS/library.js
+++ JS/library.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
         success: function(data) {
             let movies = data.results;
             console.log(movies);
-            loadRatedCards(movies);
+            loadRatedCards(movies,"TRENDING");
         } 
     })
 
     $.ajax({
-        url: 'https://api.themoviedb.org/3/trending/movie/day?api_key=33a910bf405b0e95e4f78d2f4f9b1567&primary_release_date=1990-10-12',
+        url: 'https://api.themoviedb.org/3/movie/top_rated?api_key=33a910bf405b0e95e4f78d2f4f9b1567&primary_release_date=1990-10-12',
         type: 'GET',
         dataType: 'json',
         success: function(data) {
@@ -22,7 +22,7 @@ $(document).ready(function () {
     })
 })
 
-loadRatedCards = (movies) => {
+loadRatedCards = (movies, title) => {
     $('#ratedRow').empty();
     for (let i = 0; i<8; i++) {
         const card = $(`
@@ -31,13 +31,15 @@ loadRatedCards = (movies) => {
                 <img src="https://image.tmdb.org/t/p/original${movies[i].poster_path}" class="card-img-top" alt="movie.png">
                 <div class="card-body">
                     <h3 class="card-title" id="nameText">${movies[i].title}</h3>
-                    <p>Rating: ${movies[i].vote_average}</p>
+                    <p class="ratings">Rating: ${movies[i].vote_average}</p>
                 </div>
             </div>
         </div>`)
 
         $('#ratedRow').append(card);
     }
+
+    $("#best-movies").text(title+" MOVIES");
 
     $('#ratedRow').on('click','#movieCard',function(){
         window.location.href = `../pages/movie.html?id=${$(this).find('.card').attr('data-id')}`;
@@ -62,5 +64,21 @@ loadMovieCards = (movies) => {
 
     $('#movieRow').on('click','#movieCard',function(){
         window.location.href = `../pages/movie.html?id=${$(this).find('.card').attr('data-id')}`;
+    })
+}
+
+filterChange = (filter) => {
+    let filterString = $(filter).attr("data-filter");
+    let title = $(filter).text();
+    console.log("poi");
+    $.ajax({
+        url: `https://api.themoviedb.org/3/movie/${filterString}?api_key=33a910bf405b0e95e4f78d2f4f9b1567&primary_release_date=1990-10-12`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            let movies = data.results;
+            console.log(movies);
+            loadRatedCards(movies,title);
+        } 
     })
 }
